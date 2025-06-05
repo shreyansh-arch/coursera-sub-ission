791e6ee30ba6f02a3725869752bfeea4d407a0da
shreyansh-arch <shrey.sac15@gmail.com>
2025-06-05 10:32:10 +0530

    Create  Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using EventEase;
+using EventEase.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<AttendeeService>();
+
+await builder.Build().RunAsync();
